dcac230569e9a54955d90457cfe9f6455f57c455
Takbitna <loz6xx@naver.com>
2025-04-30 01:38:27 +0900

    Update script.js
    Change product title

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // script.js
-// 모바일 네비 토글
+// 로딩 스크린 숨기기
+document.addEventListener('DOMContentLoaded', function() {
+  const loader = document.getElementById('loader');
+  loader.classList.add('hidden');
+});
+
+// 모바일 메뉴 토글
 const navToggle = document.querySelector('.nav-toggle');
 const navList = document.querySelector('.nav-list');
 navToggle.addEventListener('click', () => {
